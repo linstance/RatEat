65ee67c13b485d08e6122ee50e7fb50d87c15b19
sminyg369 <107996189+sminyg369@users.noreply.github.com>
2022-06-23 16:32:34 +0900

    04
    Bee 2022 6 23 16:32

---
+++ Assets/02.Script/BeeControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Transform target;
+
+    [Header("추적속도")]
+    [SerializeField] [Range(1f, 4f)] float moveSpeed = 3f;
+
+    [Header("근접 거리")]
+    [SerializeField] [Range(1f, 4f)] float contactDistance = 1f;
+
+    bool follow = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+
+    private void Update()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        if (Vector2.Distance(transform.position, target.position) > contactDistance && follow)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        follow = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        follow = false;
+    }
+}
