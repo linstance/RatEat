cca180a689dcaa4f6b6039eb9f972bb95618fd42
wnsrla <wnsrla@icloud.com>
2022-06-28 14:36:07 +0900

    14
    HPbar
    일단 하긴함 맛만 보셈

--- Assets/02.Script/Hpbar.cs
+++ Assets/02.Script/Hpbar.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class Hpbar : MonoBehaviour
 {
     Image Healthbar;
-    float maxHealth = 200;
+    public TextMeshProUGUI HPtext;
+    float maxHealth = 10;
     public static float health;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class Hpbar : MonoBehaviour
     void Update()
     {
         Healthbar.fillAmount = health / maxHealth;
+        HPtext.text = health.ToString() + "/" + maxHealth.ToString();
         if (health > maxHealth)
         {
             health = maxHealth;
