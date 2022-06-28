125205636f64e4429244bce8819eaff18a210d9c
sminyg369 <107996189+sminyg369@users.noreply.github.com>
2022-06-28 17:52:50 +0900

    보스 패턴 진행중 과 보스 총알 프리팹
    구현

---
+++ Assets/02.Script/BossHp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHp : MonoBehaviour
+{
+    [SerializeField]
+    private float BossHP = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/02.Script/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 1;
+    public bool isRotate;
+
+    void Update()
+    {
+        if (isRotate)
+            transform.Rotate(Vector3.forward * 3);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BorderLine") //총알이 BorderLine이라는 옵젝에 닿으면 삭제되게 만듬.
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (collision.CompareTag("Player") )
+        {
+            //collision.GetComponent<WarriorController>().Takedamage(damage);//총알이 PLayer와 닿으면 데미지가 들어가게 설정
+
+            //Destroy(gameObject);
+        }
+    }
+
+    
+}
