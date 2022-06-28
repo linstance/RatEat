a63bf88a20be0b4900fd6558606138d266e7b69c
chasoonwoo <108050850+chasoonwoo@users.noreply.github.com>
2022-06-28 12:56:35 +0900

    a

---
+++ Assets/02.Script/Hpbar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Hpbar : MonoBehaviour
+{
+    Image Healthbar;
+    float maxHealth = 200;
+    public static float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Healthbar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Healthbar.fillAmount = health / maxHealth;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+}
