781e0a5ea7e2741b5ebb493051778ae5863ddd2c
chasoonwoo <108050850+chasoonwoo@users.noreply.github.com>
2022-06-29 13:07:34 +0900

    16

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet2 : MonoBehaviour
 {
     public float speed;
     public float distance;
