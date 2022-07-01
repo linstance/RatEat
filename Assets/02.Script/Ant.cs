bf799b4f8a09323b589394075a93dba454f0591f
chasoonwoo <108050850+chasoonwoo@users.noreply.github.com>
2022-07-01 16:38:38 +0900

    개미 추격 구현
    개미 애니메이션 추가, 추격 추가

---
+++ Assets/02.Script/Ant.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+    public Transform player;
+    public float movespeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;// Start is called before the first frame update
+
+
+    Animator animator;
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+
+        animator = gameObject.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+ 
+            Debug.Log("충돌 유지");
+            animator.SetBool("IsAntHit", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Debug.Log("충돌 끝남");
+            animator.SetBool("IsAntHit", false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate()
+    {
+        moveChracter(movement);
+    }
+    void moveChracter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * movespeed * Time.deltaTime));
+    }
+
+
+}
