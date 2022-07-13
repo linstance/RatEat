1bfd3a3c45706cd3eb168612d648b519c35044fa
lDevel <allen030916@gmail.com>
2022-07-13 18:52:49 +0900

    Monster Check Mesh
    몬스터를 체크하고 방문이 열리고 닫히는 기능 구현

---
+++ Assets/CheckMash.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMash : MonoBehaviour
+{
+    /*
+      개미: Ant
+      불개미: redAnt
+      거미: Spider
+      파리: Fly
+      파리지옥 flyHell
+      벌: Bee
+    */
+
+    private GameObject OpenGate;
+    private GameObject CloseGate;
+    private int Monster = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenGate = GameObject.FindGameObjectWithTag("OpenGate");
+        CloseGate = GameObject.FindGameObjectWithTag("CloseGate");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        check();
+    }
+
+
+    void check()
+    {
+        GameObject[] Ants = GameObject.FindGameObjectsWithTag("Ant");
+        GameObject[] redAnts = GameObject.FindGameObjectsWithTag("redAnt");
+        GameObject[] Spiders = GameObject.FindGameObjectsWithTag("Spider");
+        GameObject[] Flys = GameObject.FindGameObjectsWithTag("Fly");
+        GameObject[] FlyHells = GameObject.FindGameObjectsWithTag("flyHell");
+        GameObject[] Bees = GameObject.FindGameObjectsWithTag("Bee");
+
+        if (Monster < Ants.Length || Monster < redAnts.Length || Monster < Spiders.Length || Monster < Flys.Length || Monster < FlyHells.Length || Monster < Bees.Length)
+        {
+            OpenGate.SetActive(false);
+            CloseGate.SetActive(true);
+        }
+        else if (Monster <= Ants.Length || Monster <= redAnts.Length || Monster <= Spiders.Length || Monster <= Flys.Length || Monster <= FlyHells.Length || Monster <= Bees.Length)
+        {
+            OpenGate.SetActive(true);
+            CloseGate.SetActive(false);
+        }
+    }
+   
+}
