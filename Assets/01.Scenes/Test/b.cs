512e483f1aab6db8e7f54add3dbbec2d0ec4fac7
wnsrla <wnsrla@icloud.com>
2022-07-15 03:57:44 +0900

    esc ui

---
+++ Assets/01.Scenes/Test/b.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b : MonoBehaviour
+{
+    public GameObject Pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Keep()
+    {
+        Pause.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
