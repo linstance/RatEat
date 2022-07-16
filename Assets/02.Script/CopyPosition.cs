5866f8a9833c46d9ee0b76b76b5b8bca4b3559fb
chasoonwoo <108050850+chasoonwoo@users.noreply.github.com>
2022-07-16 13:41:27 +0900

    미니맵 초안
    - 미니맵 Ui 작업 해야함
    - 로직만 구성

--- Assets/02.Script/CopyPosition.cs
+++ Assets/02.Script/CopyPosition.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class CopyPosition : MonoBehaviour
 {
-    [SerializeField]
-    private bool x, y, z;
-    [SerializeField]
-    private Transform target;
+    private GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed; // 카메라가 따라갈 속도
+    private Vector3 targetPosition; // 대상의 현재 위치
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Warrior");
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (!target) return;
-        transform.position = new Vector3(
-            (x ? target.position.x : transform.position.x),
-            (y ? target.position.y : transform.position.y),
-            (z ? target.position.z : transform.position.z));
+        // 대상이 있는지 체크
+        if (target.gameObject != null)
+        {
+            // this는 카메라를 의미 (z값은 카메라값을 그대로 유지)
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            // vectorA -> B까지 T의 속도로 이동
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
     }
 }
