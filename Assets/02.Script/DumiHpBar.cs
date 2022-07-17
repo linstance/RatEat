fe944015cc29f924219e195e855ea879cb54fe30
wnsrla <wnsrla@icloud.com>
2022-07-17 23:17:58 +0900

    허수아비

---
+++ Assets/02.Script/DumiHpBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DumiHpBar : MonoBehaviour
+{
+    Image DumiHp;
+    public static int DMHealth;
+    public static int DCHealth;// Start is called before the first frame update
+    void Start()
+    {
+        DMHealth += 10;
+        DumiHp = GetComponent<Image>();
+        DCHealth = DMHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DumiHp.fillAmount = DCHealth / 10;
+
+        if(DCHealth <= 0)
+        {
+            DCHealth += DMHealth;
+        }
+    }
+}
