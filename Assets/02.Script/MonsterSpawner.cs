57274fb6d10d4e4347bec39e20f069928a55352c
lDevel <allen030916@gmail.com>
2022-07-18 16:41:59 +0900

    StartPoint
    Set

---
+++ Assets/02.Script/MonsterSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject Monster1;
+    public GameObject Monster2;
+    public GameObject Monster3;
+    public GameObject Monster4;
+    public GameObject Monster5;
+    public GameObject Monster6;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("MonsterSet", 0.5f);
+        }
+    }
+
+    void MonsterSet()
+    {
+        Monster1.SetActive(true);
+        Monster2.SetActive(true);
+        Monster3.SetActive(true);
+        Monster4.SetActive(true);
+        Monster5.SetActive(true);
+        Monster6.SetActive(true);
+    }
+
+}
