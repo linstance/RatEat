dd3c6f545d712bb869b950b5c6ed71bac182c934
sminyg369 <107996189+sminyg369@users.noreply.github.com>
2022-07-18 18:51:39 +0900

    그냥
    ㅇㅇㅇㅇ

---
+++ Assets/02.Script/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" )
+        {
+            //SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
